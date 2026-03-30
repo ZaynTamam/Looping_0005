2df3ed081d671069bbebbd846869592abc9caf2e
ZaynTamam <tamam.zain78@gmail.com>
2026-03-30 12:28:26 +0700

    Add second main function to calculate average of two grades

--- Looping.cpp
+++ Looping.cpp
@@ -40,3 +40,19 @@ int main()
     cout << "Bilangan acak do while yang terakhir = " << x << endl;
     cout << endl;
 }
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    float nilM, nilB, rerata;
+    string status;
+
+    cout << "Masukkan nilai Matematika = ";
+    cin >> nilM;
+    cout << "Masukkan nilai Bahasa Inggris = ";
+    cin >> nilB;
+
+    rerata = (nilM + nilB) / 2;
+}
