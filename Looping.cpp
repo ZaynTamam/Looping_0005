a6136351129aa74176d5446cdc819a430c7bbf98
ZaynTamam <tamam.zain78@gmail.com>
2026-03-30 12:30:24 +0700

    Refactor main function to remove duplicate code for average grade calculation

--- Looping.cpp
+++ Looping.cpp
@@ -63,3 +63,19 @@ int main()
 
     cout << "Status kelulusan = " << status << ", dengan nilai rata-rata = " << rerata << endl;
 }
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    float nilM, nilB, rerata;
+    string status;
+
+    cout << "Masukkan nilai Matematika = ";
+    cin >> nilM;
+    cout << "Masukkan nilai Bahasa Inggris = ";
+    cin >> nilB;
+
+    rerata = (nilM + nilB) / 2;
+}
